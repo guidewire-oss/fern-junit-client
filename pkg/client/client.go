cb041777a79245e627e8c3476652621446c44e50
Monforton <113210196+Monforton@users.noreply.github.com>
2024-12-05 15:50:55 -0600

    feat: set testSeed as now in nano (#11)

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"log"
+	"time"
 
 	"github.com/guidewire-oss/fern-junit-client/pkg/models/fern"
 )
@@ -9,6 +10,7 @@ import (
 func SendReports(fernUrl, projectName, filePattern string, verbose bool) error {
 	var testRun fern.TestRun
 	testRun.TestProjectName = projectName
+	testRun.TestSeed = uint64(time.Now().Nanosecond())
 
 	log.Default().Println("Parsing reports...")
 	if err := parseReports(&testRun, filePattern, verbose); err != nil {
